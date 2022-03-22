a04b74554edbf8403ff98f9f6da8647e8b6135a5
mel0778 <101048472+mel0778@users.noreply.github.com>
2022-03-23 06:01:57 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -15,7 +15,7 @@ def login(request):
         with connection.cursor() as cursor:
 
             cursor.execute("SELECT password FROM buyer WHERE username = %s", [request.POST['username']])
-	    password = cursor.fetchone()
+            password = cursor.fetchone()
             if password == %s, [request.POST['password']]:
                 messages.success(request, f'Welcome user %s back to HONUSupper!' % (request.POST['username']))
                 return redirect('loginhome')    
