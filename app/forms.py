063cff74f82d14875fada799de84a210f126593e
mel0778 <101048472+mel0778@users.noreply.github.com>
2022-03-23 03:13:28 +0800

    Add files via upload

---
+++ app/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from .models import Ride, Bid
+		
+class CreateUserForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+class Advertise(forms.ModelForm):
+    class Meta:
+        model = Ride
+        fields = ["ride_id", "origin", "destination", "start_time", "driver"]
+
+class Bid(forms.ModelForm):
+    class Meta:
+        model = Bid
+        fields = ["bid_id", "username", "ride", "bid_price"]
+
