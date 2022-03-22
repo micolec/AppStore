dd6b499810cd763a8ffc973151e6463020826e22
mel0778 <101048472+mel0778@users.noreply.github.com>
2022-03-22 21:26:26 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -9,14 +9,15 @@ def index(request):
     if request.POST:
         if request.POST['action'] == 'delete':
             with connection.cursor() as cursor:
-                cursor.execute("DELETE FROM customers WHERE customerid = %s", [request.POST['id']])
+                cursor.execute("DELETE FROM buyer WHERE username = %s", [request.POST['id']])
 
     ## Use raw query to get all objects
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM customers ORDER BY customerid")
-        customers = cursor.fetchall()
+        cursor.execute("SELECT * FROM buyer ORDER BY first_name")
+        buyers = cursor.fetchall()
+        # list of tuples
 
-    result_dict = {'records': customers}
+    result_dict = {'records': buyers}
 
     return render(request,'app/index.html',result_dict)
 
@@ -26,9 +27,9 @@ def view(request, id):
     
     ## Use raw query to get a customer
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM customers WHERE customerid = %s", [id])
-        customer = cursor.fetchone()
-    result_dict = {'cust': customer}
+        cursor.execute("SELECT * FROM buyer WHERE username = %s", [id])
+        buyer = cursor.fetchone()
+    result_dict = {'buyer': buyer}
 
     return render(request,'app/view.html',result_dict)
 
@@ -42,17 +43,17 @@ def add(request):
         ## Check if customerid is already in the table
         with connection.cursor() as cursor:
 
-            cursor.execute("SELECT * FROM customers WHERE customerid = %s", [request.POST['customerid']])
-            customer = cursor.fetchone()
+            cursor.execute("SELECT * FROM buyer WHERE username = %s", [request.POST['username']])
+            buyer = cursor.fetchone()
             ## No customer with same id
-            if customer == None:
+            if buyer == None:
                 ##TODO: date validation
-                cursor.execute("INSERT INTO customers VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                        , [request.POST['first_name'], request.POST['last_name'], request.POST['email'],
-                           request.POST['dob'] , request.POST['since'], request.POST['customerid'], request.POST['country'] ])
+                cursor.execute("INSERT INTO buyer VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                        , [request.POST['username'], request.POST['password'], request.POST['first_name'],
+                           request.POST['last_name'] , request.POST['phone_number'], request.POST['hall'], request.POST['wallet_balance'] ])
                 return redirect('index')    
             else:
-                status = 'Customer with ID %s already exists' % (request.POST['customerid'])
+                status = 'Buyer with Username %s already exists' % (request.POST['username'])
 
 
     context['status'] = status
@@ -69,7 +70,7 @@ def edit(request, id):
 
     # fetch the object related to passed id
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM customers WHERE customerid = %s", [id])
+        cursor.execute("SELECT * FROM buyer WHERE username = %s", [id])
         obj = cursor.fetchone()
 
     status = ''
@@ -78,11 +79,11 @@ def edit(request, id):
     if request.POST:
         ##TODO: date validation
         with connection.cursor() as cursor:
-            cursor.execute("UPDATE customers SET first_name = %s, last_name = %s, email = %s, dob = %s, since = %s, country = %s WHERE customerid = %s"
-                    , [request.POST['first_name'], request.POST['last_name'], request.POST['email'],
-                        request.POST['dob'] , request.POST['since'], request.POST['country'], id ])
-            status = 'Customer edited successfully!'
-            cursor.execute("SELECT * FROM customers WHERE customerid = %s", [id])
+            cursor.execute("UPDATE buyer SET password = %s, first_name = %s, last_name = %s, phone_number = %s, hall = %s, wallet_balance = %s WHERE username = %s"
+                    , [request.POST['password'], request.POST['first_name'], request.POST['last_name'],
+                        request.POST['phone_number'] , request.POST['hall'], request.POST['wallet_balance'], id ])
+            status = 'Buyer edited successfully!'
+            cursor.execute("SELECT * FROM buyer WHERE username = %s", [id])
             obj = cursor.fetchone()
 
 
