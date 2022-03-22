d659bb252745f050b7c149263bc4bdc696e5dd03
micolec <86700792+micolec@users.noreply.github.com>
2022-03-22 21:18:11 +0800

    Add files via upload

---
+++ sql/SupperShop.sql
@@ -0,0 +1,4 @@
+insert into shop (username, password, shopname, opening, closing, delivery_fee) values ('formosa123', 'PhWdIUojpUH', 'Formosa Delights', '19:00:00', '22:30:00',13);
+insert into shop (username, password, shopname, opening, closing, delivery_fee) values ('alamaan', 'bBz8SYQ7', 'Al Amaan Restaurant', '19:00:00', '22:00:00', 16);
+insert into shop (username, password, shopname, opening, closing, delivery_fee) values ('supersnacksss', '0qgSRN', 'Super Snacks', '18:00:00', '23:00:00', 19);
+insert into shop (username, password, shopname, opening, closing, delivery_fee) values ('fongseng88', '9lE2xay4FllU', 'Fong Seng', '17:00:00', '23:30:00',14);
