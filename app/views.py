c549652c37f0c7971f812adb2fa57cc62ae65ac6
micolec <86700792+micolec@users.noreply.github.com>
2022-03-23 21:07:48 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -133,20 +133,19 @@ def addgrouporder(request):
     if request.POST:
         ## Check if customerid is already in the table
         with connection.cursor() as cursor:
-
-            cursor.execute("SELECT * FROM orderid WHERE creator = %s AND hall = %s AND shopname = %s AND order_date = %s AND order_by = %s", [request.POST['creator'], request.POST['hall'], request.POST['shopname'],request.POST['order_date'], request.POST['order_by']])
-            orderid = cursor.fetchone()
-            ## No orderid with same details
-            if orderid == None:
-                cursor.execute("SELECT * FROM shop WHERE shopname = %s", [request.POST['shopname']])
-	        shopdet = cursor.fetchone()
-                opening = shopdet[3]
-                closing = shopdet[4]
-                cursor.execute("SELECT * FROM shop WHERE shopname = %s", [request.POST['shopname']])
-                messages.success(request, f'New Group Order created for %s! Please remember to close and send your group order.' % (request.POST['creator']))
-                return redirect('index')
-            else:
-                status = '%s Group Order created by Username %s already exists' % (request.POST['shopname'], request.POST['creator'])
+		cursor.execute("SELECT * FROM orderid WHERE creator = %s AND hall = %s AND shopname = %s AND order_date = %s AND order_by = %s", [request.POST['creator'], request.POST['hall'], request.POST['shopname'],request.POST['order_date'], request.POST['order_by']])
+		orderid = cursor.fetchone()
+## No orderid with same details
+		if orderid == None:
+			cursor.execute("SELECT * FROM shop WHERE shopname = %s", [request.POST['shopname']])
+			shopdet = cursor.fetchone()
+			opening = shopdet[3]
+			closing = shopdet[4]
+			cursor.execute("SELECT * FROM shop WHERE shopname = %s", [request.POST['shopname']])
+			messages.success(request, f'New Group Order created for %s! Please remember to close and send your group order.' % (request.POST['creator']))
+			return redirect('index')
+		else:
+			status = '%s Group Order created by Username %s already exists' % (request.POST['shopname'], request.POST['creator'])
 
 
     context['status'] = status
