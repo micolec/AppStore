493d8b839499ebd10993a512b4eddf389d8c129b
vitorio-p <vitorio.p@u.nus.edu>
2022-03-25 11:38:27 +0800

    Debugging seller_orderid

--- app/views.py
+++ app/views.py
@@ -198,11 +198,10 @@ def seller_orderid(request, id):
 
     if request.POST:
         with connection.cursor() as cursor:
-            cursor.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                    , [request.POST['username'], hall, group_ord_id, hall, shopname, request.POST['item'], request.POST['qty'] ])
-            messages.success(request, f'%s added to Group Order! Feel free to order more items.' % (request.POST['item']))
-            return redirect(f'/viewindivorder/%s' % (request.POST['username']))
-            """should link to viewindivorder"""
+            cursor.execute("UPDATE delivery_status SET %s"
+                    , request.POST['delivery_status'])
+            messages.success(request, f'Delivery Status has been updated!' % (request.POST['item']))
+            return redirect(f'/sellerindex')
  
     return render(request, "app/seller_orderid.html", result_dict)
 
