5e888bee93c306b11c0e91abaf474ae50c691d8d
mel0778 <101048472+mel0778@users.noreply.github.com>
2022-03-31 03:37:12 +0800

    login

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.contrib.auth.models import User
 
 # class AuthGroup(models.Model):
 #     name = models.CharField(unique=True, max_length=150)
@@ -172,6 +173,8 @@ from django.db import models
 
 
 class Buyer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="buyer")
+    name = models.CharField(max_length=50, blank=True, null=True)
     username = models.CharField(primary_key=True, max_length=32)
     password = models.CharField(max_length=32)
     first_name = models.CharField(max_length=128)
