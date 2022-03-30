db47f048a457e9d81408ec578721473ff0d143f3
micolec <86700792+micolec@users.noreply.github.com>
2022-03-31 00:00:15 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -127,14 +127,14 @@ def viewindivorder(request, id):
     ## Delete customer NEED TO FIX!!!! must add condition on item also
     context = {}
     status = ''
-   
+    
     with connection.cursor() as cursor:
-        cursor.execute("SELECT username, buyer_hall, group_order_id, o.shopname, o.item, qty, price, (price*qty) AS total_price FROM orders o, item i WHERE o.shopname = i.shopname AND o.item=i.item AND username = %s", [id])
+        cursor.execute("SELECT username, buyer_hall, group_order_id, o.shopname, o.item, qty, price, (price*qty) AS total_price FROM orders o, item i WHERE o.shopname = i.shopname AND o.item=i.item AND username = %s" , [id])
         indivorders = cursor.fetchall()
         grpid = indivorders[0][2]
         # list of tuples
     with connection.cursor() as cursor:
-        cursor.execute(";with t1 as (SELECT group_order_id, SUM(total_price) AS group_total, ROUND((delivery_fee *1.0)/ COUNT(*), 2) AS delivery_fee_per_pax FROM (SELECT o.group_order_id, (price * qty) AS total_price, delivery_fee FROM orders o, item i, shop s WHERE o.shopname = i.shopname AND o.shopname = s.shopname AND o.item = i.item) AS orders_with_price GROUP BY group_order_id, delivery_fee), t2 as ( SELECT username, group_order_id, SUM(total_price) AS indiv_total FROM (SELECT username, group_order_id,(price * qty) AS total_price FROM orders o, item i WHERE o.shopname = i.shopname AND o.item = i.item) AS orders_with_price GROUP BY username, group_order_id ORDER BY group_order_id) SELECT t2.username, t2.group_order_id, t2.indiv_total, t1.delivery_fee_per_pax,(t2.indiv_total + CAST(t1.delivery_fee_per_pax AS MONEY)) AS Total FROM t1,t2 WHERE t1.group_order_id = t2.group_order_id AND t2.username = %s ORDER BY group_order_id DESC", [id])
+        cursor.execute(";with t1 as (SELECT group_order_id, SUM(total_price) AS group_total, ROUND((delivery_fee *1.0)/ COUNT(*), 2) AS delivery_fee_per_pax FROM (SELECT o.group_order_id, (price * qty) AS total_price, delivery_fee FROM orders o, item i, shop s WHERE o.shopname = i.shopname AND o.shopname = s.shopname AND o.item = i.item) AS orders_with_price GROUP BY group_order_id, delivery_fee), t2 as ( SELECT username, group_order_id, SUM(total_price) AS indiv_total FROM (SELECT username, group_order_id,(price * qty) AS total_price FROM orders o, item i WHERE o.shopname = i.shopname AND o.item = i.item) AS orders_with_price GROUP BY username, group_order_id ORDER BY group_order_id) SELECT t2.username, t2.group_order_id, t2.indiv_total, t1.delivery_fee_per_pax,(t2.indiv_total + CAST(t1.delivery_fee_per_pax AS MONEY)) AS Total FROM t1,t2 WHERE t1.group_order_id = t2.group_order_id AND t2.username = %s AND t1.group_order_id = %s ORDER BY group_order_id DESC", [id,grpid])
         fee = cursor.fetchall()
         total = fee[0][4]
         total = float(total[1:])
