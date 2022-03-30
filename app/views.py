abcc547c05aa2ea2d3ac4900c43f4a43ba4070b7
micolec <86700792+micolec@users.noreply.github.com>
2022-03-30 18:30:17 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -132,7 +132,7 @@ def viewindivorder(request, id):
 
     ## Use raw query to get all objects
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM orders WHERE username = %s", [id])
+        cursor.execute("SELECT username, buyer_hall, group_order_id, o.shopname, o.item, qty, price, (price*qty) AS total_price FROM orders o, item i WHERE o.shopname = i.shopname AND o.item=i.item AND username = %s", [id])
         indivorders = cursor.fetchall()
         grpid = indivorders[0][2]
         # list of tuples
