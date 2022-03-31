d6e1a090fc90018b504033c1246ebac37550d979
mel0778 <101048472+mel0778@users.noreply.github.com>
2022-03-31 14:19:40 +0800

    models

--- app/models.py
+++ app/models.py
@@ -37,21 +37,21 @@ class AuthPermission(models.Model):
         unique_together = (('content_type', 'codename'),)
 
 
-# class AuthUser(models.Model):
-#     password = models.CharField(max_length=128)
-#     last_login = models.DateTimeField(blank=True, null=True)
-#     is_superuser = models.BooleanField()
-#     username = models.CharField(unique=True, max_length=150)
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=150)
-#     email = models.CharField(max_length=254)
-#     is_staff = models.BooleanField()
-#     is_active = models.BooleanField()
-#     date_joined = models.DateTimeField()
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
 
-#     class Meta:
-#         managed = False
-#         db_table = 'auth_user'
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
 
 
 class AuthUserGroups(models.Model):
