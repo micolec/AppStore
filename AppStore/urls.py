586f26025c8078d5a0ee554718f4053f6276b36d
mel0778 <101048472+mel0778@users.noreply.github.com>
2022-03-31 20:54:28 +0800

    migrations

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 import app.views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('', app.views.index, name='index'),
     path('login', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
     path('loginseller', app.views.loginseller, name = 'loginseller'),
