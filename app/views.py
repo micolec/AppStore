530dbbc1952e21110e4aff37115adc2c05226521
micolec <86700792+micolec@users.noreply.github.com>
2022-04-06 01:44:22 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -366,6 +366,7 @@ def viewindivorder(request, id):
                 cursor.execute("DELETE FROM orders WHERE username = %s", [id])
         if request.POST['action'] == 'deduct':
             with connection.cursor() as cursor:
+                total = request.POST['total']
                 if (existing - total) >= 5:
                     cursor.execute("UPDATE buyer SET wallet_balance = (%s - %s) WHERE username = %s", [existing, total, id])
                     messages.success(request, f'Paid! Wallet Balance has been updated.')
