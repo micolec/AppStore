35a4c3314ecaa81ad58630d1d3eedddee7ed2229
vitorio-p <vitorio.p@u.nus.edu>
2022-04-07 05:35:42 +0800

    Fixed add_menu for seller

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -47,7 +47,7 @@ urlpatterns = [
     path('sellerindex/seller_orderid/<str:id>', app.views.seller_orderid, name='seller_orderid'),
     path('seller_menu/<str:shopname>', app.views.seller_menu, name='seller_menu'),
     path('seller_menu/edit_menu/<str:item>', app.views.edit_menu, name='edit_menu'),
-    path('add_menu/<str:shopname>', app.views.add_menu, name='add_menu'),
+    path('seller_menu/add_menu/<str:shopname>', app.views.add_menu, name='add_menu'),
 
     #SUPERADMIN
     path('buyerindex', app.views.buyerindex, name='buyerindex'),
--- app/views.py
+++ app/views.py
@@ -701,6 +701,7 @@ def sellerindex(request, shopname):
     if request.POST:
         if request.POST['action'] == 'edit':
             return redirect(f'/seller_orderid/%s' % id)
+    
     result_dict['shopname'] = shopname
 
     return render(request, "app/sellerindex.html", result_dict)
@@ -751,23 +752,14 @@ def seller_menu(request, shopname):
 
     return render(request,"app/seller_menu.html",result_dict)
 
-def add_menu(request, id):
-    with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM orderid WHERE group_order_id = %s", [id])
-            prev = cursor.fetchone()
-            group_ord_id = prev[0]
-            hall = prev[2]
-            shopname = prev[3]
-            result_dict = {'prev': prev}
-
+def add_menu(request, shopname):
     if request.POST:
         with connection.cursor() as cursor:
             cursor.execute("INSERT INTO item VALUES (%s, %s, %s)"
-                    , id, request.POST['item'], request.POST['price'])
+                ,   [shopname, request.POST['item'], request.POST['price']])
             messages.success(request, f'%s has been added into the menu!' % (request.POST['item']))
-            return redirect(f'/seller_menu/%s' % (id))
- 
-    return render(request, "app/add_menu.html", result_dict)
+            return redirect(f'/seller_menu/%s' % (shopname))
+    return render(request, "app/add_menu.html")
 
 def edit_menu(request, item):
     with connection.cursor() as cursor:
